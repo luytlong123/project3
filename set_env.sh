1395954d103d846386de81d88526070eb31b7fc4
tranquocnha <tranquocnha1808000@gmail.com>
2022-11-13 19:51:02 +0700

    update project 3

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=project3udagram
+export POSTGRES_PASSWORD=project3udagram
+export POSTGRES_HOST=project3udagram.c4yfkyqlfjl6.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=project3udagram
+export AWS_BUCKET=project-3-s3
+export AWS_REGION=us-east-1a
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
